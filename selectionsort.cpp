86bab766e74a45383789e2526ef7e1d3bff6213a
Shruti Rajendra Nahar <84951018+shruti-nahar14@users.noreply.github.com>
2021-10-01 10:52:32 +0530

    Implementation of selection sort

--- selectionsort.cpp
+++ selectionsort.cpp
@@ -1,3 +1,9 @@
+/*input:- arr[]={10,30,45,7,8,9}
+output:-  After sorting   
+          7 8 9 10 30 45 
+description: Implementation of selection sort
+date: 01-10-2021
+Author name: Shruti Nahar*/
 #include<iostream>
 using namespace std;
 void selectionsort(int arr[],int n)
