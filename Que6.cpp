7b56bd03240fb76002a2137a9023bea143c0da7d
Shruti Rajendra Nahar <84951018+shruti-nahar14@users.noreply.github.com>
2021-10-05 09:57:48 +0530

    Implementation of new string by alternately combining the characters of two halves of the string in reverse

--- Que6.cpp
+++ Que6.cpp
@@ -1,3 +1,9 @@
+/*input:- sunshine
+output:- sennuish
+description: Implementation of new string by alternately combining the characters of two halves of the string in reverse
+date: 05-10-2021
+Author name: Shruti Nahar*/
+
 #include<iostream>
 using namespace std;
 void halfword (string s)
