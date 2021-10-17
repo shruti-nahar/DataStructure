37ed98cb9f2bbf7133143e2264698047b6342b00
Shruti Rajendra Nahar <84951018+shruti-nahar14@users.noreply.github.com>
2021-10-17 22:15:34 +0530

    Implementation of DNF Sort

---
+++ dnfsort.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+using namespace std;
+void dnfsort(int arr[],int n)
+{
+   int low=0;
+   int mid=0;
+   int high=n-1;
+   while(mid<=high)
+   {
+       if(arr[mid]==0)
+       {
+           swap(arr[low],arr[mid]);
+           low++;
+           mid++;
+       }
+       else if(arr[mid]==1)
+       {
+           mid++;
+       }
+       else if(arr[mid]==2)
+       {
+           swap(arr[mid],arr[high]);
+           high--;
+       }
+   }
+}
+int main()
+{
+    int arr[]={1,1,0,0,1,2,2};
+    int n=sizeof(arr)/sizeof(arr[0]);
+    dnfsort(arr,n);
+    cout<<"After DNF Sort ";
+    for(int i=0;i<n;i++)
+    {
+        cout<<arr[i]<<" ";
+    }
+}
